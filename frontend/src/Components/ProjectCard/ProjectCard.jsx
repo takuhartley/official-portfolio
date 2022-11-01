ce127027beca2704d818911c64894abd60ae6a07
Taku Hartley <43013927+takuhartley@users.noreply.github.com>
2022-11-01 23:53:15 +0900

    Updated Project Card
    Updated project card and added the fields

--- frontend/src/Components/ProjectCard/ProjectCard.jsx
+++ frontend/src/Components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './ProjectCard.scss'
 const ProjectCard = ({ project }) => {
+  console.log(project)
   return (
     <>
-      <div className='project-card'>{project.title}</div>
+      <div className='project-card'>
+        <p>{project._id}</p>
+        <h1>{project.title}</h1>
+        <h3>{project.subTitle}</h3>
+        <p>{project.description}</p>
+        <img src={project.images.thumbnail} alt={project.title} />
+        <a href={project.links.websiteUrl}>{project.links.websiteUrl}</a>
+        <p>Tags {project.tags}</p>
+      </div>
     </>
   )
 }
