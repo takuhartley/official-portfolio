18d83b5949f6c9c4b185ab8016300bf3ae930154
Taku Hartley <43013927+takuhartley@users.noreply.github.com>
2022-12-02 08:59:42 +0900

    Added ProjectDetails Reducer

--- frontend/src/Redux/Reducers/projectReducers.js
+++ frontend/src/Redux/Reducers/projectReducers.js
@@ -1,7 +1,10 @@
 import {
   PROJECT_LIST_FAIL,
   PROJECT_LIST_REQUEST,
-  PROJECT_LIST_SUCCESS
+  PROJECT_LIST_SUCCESS,
+  PROJECT_DETAIL_FAIL,
+  PROJECT_DETAIL_REQUEST,
+  PROJECT_DETAIL_SUCCESS
 } from '../Constants/projectConstants'
 export const projectListReducer = (state = { projects: [] }, action) => {
   switch (action.type) {
@@ -10,6 +13,18 @@ export const projectListReducer = (state = { projects: [] }, action) => {
     case PROJECT_LIST_SUCCESS:
       return { loading: false, projects: action.payload }
     case PROJECT_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+export const projectDetailsReducer = (state = { project: {} }, action) => {
+  switch (action.type) {
+    case PROJECT_DETAIL_REQUEST:
+      return { loading: true, ...state }
+    case PROJECT_DETAIL_SUCCESS:
+      return { loading: false, project: action.payload }
+    case PROJECT_DETAIL_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
